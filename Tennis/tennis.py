21bb3fa85f2e5d5238503a5905da9a208cfa1644
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-21 18:23:46 +1000

    Removed redundant code

--- Tennis/tennis.py
+++ Tennis/tennis.py
@@ -67,34 +67,12 @@ class TennisGameDefactored2:
             P1res = tennisScore[self.p1Points]
         if self.p2Points < 4:
             P2res = tennisScore[self.p2Points]
-
+            
         if P1res == P2res and P1res == '':
             result = 'Deuce'
         elif P1res == P2res:
             result = f'{P1res}-All'
         else:
-            result = P1res + "-" + P2res
-        
-        
-        if (self.p1Points>self.p2Points and self.p1Points < 4):
-            if (self.p1Points==2):
-                P1res="Thirty"
-            if (self.p1Points==3):
-                P1res="Forty"
-            if (self.p2Points==1):
-                P2res="Fifteen"
-            if (self.p2Points==2):
-                P2res="Thirty"
-            result = P1res + "-" + P2res
-        if (self.p2Points>self.p1Points and self.p2Points < 4):
-            if (self.p2Points==2):
-                P2res="Thirty"
-            if (self.p2Points==3):
-                P2res="Forty"
-            if (self.p1Points==1):
-                P1res="Fifteen"
-            if (self.p1Points==2):
-                P1res="Thirty"
             result = P1res + "-" + P2res
         
         if (self.p1Points > self.p2Points and self.p2Points >= 3):
