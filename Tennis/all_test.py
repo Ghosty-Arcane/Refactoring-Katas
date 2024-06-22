30e8bc926638193ea2b0c570c80348adfab7b7b0
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-23 05:55:51 +1000

    Unit testing bug fixed

--- Tennis/all_test.py
+++ Tennis/all_test.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from tennis import TennisGame
+from tennis_unittest import test_cases, play_game
 from yahtzee import Yahtzee
 
-# These unit tests can be run using the py.test framework
-# available from http://pytest.org/
+class TestTennis:
+
+    @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
+    def test_get_score(self, p1Points, p2Points, score, p1Name, p2Name):
+        game = play_game(p1Points, p2Points, p1Name, p2Name)
+        assert score == game.score()
 
 def test_chance_scores_sum_of_all_dice():
         expected = 15
