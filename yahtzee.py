7f30ac59ad49edee236930cf6a8ec65a89454555
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-23 07:45:57 +1000

    Added rules enum

--- yahtzee.py
+++ yahtzee.py
@@ -1,3 +1,22 @@
+from enum import Enum, auto
+
+class Rules(Enum):
+    CHANCE = auto()
+    YAHTZEE = auto()
+    ONES = auto()
+    TWOS = auto()
+    THREES = auto()
+    FOURS = auto()
+    FIVES = auto()
+    SIXES = auto()
+    PAIR = auto()
+    TWO_PAIR = auto()
+    THREE_KIND = auto()
+    SMALL_STRAIGHT = auto()
+    LARGE_STRAIGHT = auto()
+    FULL_HOUSE = auto()
+    
+
 class YahtzeeScore:
     @staticmethod
     def chance(dice: list[int]) ->int:
@@ -50,6 +69,10 @@ class YahtzeeScore:
         trio = YahtzeeScore.of_a_kind(3, dice)
         if pair != 0 and trio != 0:
             return pair + trio
+        return 0
+    
+    @staticmethod
+    def Score(selection, dice: list[int]) ->int:
         return 0
     
 
@@ -119,7 +142,6 @@ class Yahtzee:
         if YahtzeeScore.straight(2, [d1, d2, d3, d4, d5]):
             return 20
         return 0
-    
 
     @staticmethod
     def fullHouse(d1: int, d2: int, d3: int, d4: int, d5: int) ->int:
