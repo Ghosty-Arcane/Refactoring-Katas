235a5e0813f143e98d6ec41850ddcd6842a38994
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-23 06:47:09 +1000

    5s now use count

--- yahtzee.py
+++ yahtzee.py
@@ -48,15 +48,9 @@ class Yahtzee:
     
     def fours(self) ->int:
         return YahtzeeScore.count(4, self.dice)
-    
 
     def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)): 
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
+        return YahtzeeScore.count(5, self.dice)
     
 
     def sixes(self):
