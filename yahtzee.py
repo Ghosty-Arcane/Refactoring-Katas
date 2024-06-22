ae2639681617bbaea7e2e4259ca97463e06ad51a
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-23 06:27:39 +1000

    Implemented YahtzeeScore class
    All outside code should begin migrating to new class
    Current class will remain as API surface for legacy
    Chance redirect active

--- yahtzee.py
+++ yahtzee.py
@@ -1,14 +1,14 @@
+class YahtzeeScore:
+    @staticmethod
+    def chance(dice: list[int,int,int,int,int]) ->int:
+        total = sum(dice)
+        return total
+    
 class Yahtzee:
 
     @staticmethod
     def chance(d1: int, d2: int, d3: int, d4: int, d5: int) ->int:
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+        return YahtzeeScore.chance([d1, d2, d3, d4, d5])
 
     @staticmethod
     def yahtzee(dice):
