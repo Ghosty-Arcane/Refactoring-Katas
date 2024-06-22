edbc3064ea83037abf11e46f7924298d3643c938
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-23 07:12:27 +1000

    one_pair swapped to of_a_kind

--- yahtzee.py
+++ yahtzee.py
@@ -84,7 +84,7 @@ class Yahtzee:
     
     @staticmethod
     def score_pair(d1: int, d2: int, d3: int, d4: int, d5: int) ->int:
-        return YahtzeeScore.one_pair([d1, d2, d3, d4, d5])
+        return YahtzeeScore.of_a_kind(2, [d1, d2, d3, d4, d5])
     
     @staticmethod
     def two_pair(d1: int, d2: int, d3: int, d4: int, d5: int) ->int:
