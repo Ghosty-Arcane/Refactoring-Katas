f3f78403e466af39ae307d4f0adebc0ec788a35a
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-23 07:38:47 +1000

    Type annoting largeStraight properly

--- yahtzee.py
+++ yahtzee.py
@@ -43,6 +43,7 @@ class YahtzeeScore:
                 return False
             compare += 1
         return True
+    
 
 class Yahtzee:
     def __init__(self, d1: int, d2: int, d3: int, d4: int, _5: int):
@@ -106,7 +107,7 @@ class Yahtzee:
         return 0
 
     @staticmethod
-    def largeStraight(d1: int, d2: int, d3: int, d4: int, d5: int):
+    def largeStraight(d1: int, d2: int, d3: int, d4: int, d5: int) ->int:
         if YahtzeeScore.straight(2, [d1, d2, d3, d4, d5]):
             return 20
         return 0
